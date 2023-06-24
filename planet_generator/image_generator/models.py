f99be41a8ecdfaf137b88b33675fa77f63cd28d6
PavloShutz <pavlo050706@gmail.com>
2023-06-24 13:19:33 +0300

    created model planet and made first migration

--- planet_generator/image_generator/models.py
+++ planet_generator/image_generator/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_albedo(albedo):
+    """Validate if albedo's values range is from 0 to 1."""
+    if Decimal(0) > albedo or Decimal(1) < albedo:
+        raise ValidationError(
+            _("%(albedo)s is not in range [0, 1]"),
+            params={"albedo": albedo},
+        )
+
+
+class Planet(models.Model):
+    """Generated planet by user."""
+
+    name = models.CharField(
+        max_length=150,
+        help_text="Name of the planet.",
+    )
+    radius = models.DecimalField(
+        max_digits=11,
+        decimal_places=4,
+        db_column="mean_radius",
+        help_text="Mean radius of the planet.",
+    )
+    mass = models.DecimalField(
+        max_digits=10,
+        decimal_places=5,
+        help_text="Total mass of the planet.",
+    )
+    bulk_density = models.DecimalField(
+        max_digits=5,
+        decimal_places=4,
+        help_text="Density computed using the total volume and mass of the planet.",
+    )
+    albedo_help_text = """
+        Albedo is ratio of the light received by a body to the light reflected by that body.
+        Albedo values range from 0 (pitch black) to 1 (perfect reflector).
+    """
+    albedo = models.DecimalField(
+        max_digits=5,
+        decimal_places=4,
+        help_text=albedo_help_text,
+        validators=[validate_albedo],
+    )
+    gravity = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        help_text="The gravitational acceleration on the planet's surface at the equator.",
+    )
+
+    def __str__(self):
+        """Return name of the planet."""
+        return self.name
