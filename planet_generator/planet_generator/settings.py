1cae6c1eac53a4f0c0013d29472ed9119ba6671c
PavloShutz <pavlo050706@gmail.com>
2023-06-24 12:25:00 +0300

    initialized new app

--- planet_generator/planet_generator/settings.py
+++ planet_generator/planet_generator/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'image_generator',
+
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
