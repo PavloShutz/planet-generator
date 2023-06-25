99e4a765b45f6d7f7ab8021a3313fe588226c828
PavloShutz <pavlo050706@gmail.com>
2023-06-25 20:36:20 +0300

    added simple auth system

--- planet_generator/users/urls.py
+++ planet_generator/users/urls.py
@@ -1,1 +1,11 @@
 """URL patterns for 'users' app."""
+
+from django.urls import path, include
+
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    path('', include("django.contrib.auth.urls")),
+    path('register/', views.register, name="register"),
+]
