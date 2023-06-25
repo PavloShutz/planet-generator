61a0b4595d0ee7e19fd2c370486c99ba9caaedfd
PavloShutz <pavlo050706@gmail.com>
2023-06-25 10:14:36 +0300

    updated project's settings

--- planet_generator/planet_generator/settings.py
+++ planet_generator/planet_generator/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
+OPENAI_TOKEN = os.getenv('OPENAI_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
